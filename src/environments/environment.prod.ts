56d3286357ee3adc4a8afdb873599d4c035fb365
dev-thiago <dev-thiago@localhost.localdomain>
2017-11-02 23:07:15 +0100

    fixing base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // BASE_URL: 'https://angular-algaworks-thiago.herokuapp.com',
-  BASE_URL: 'http://localhost:8080'
+  BASE_URL: 'https://angular-algaworks-thiago.herokuapp.com'
+  // BASE_URL: 'http://localhost:8080'
 };
